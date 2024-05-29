8a17d85d866fc2b7b75b0c2950c3025df3f3d5b2
Eyohen <40663724+Eyohen@users.noreply.github.com>
2024-05-29 14:49:15 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,9 +55,9 @@ const Home = () => {
  
     <>
     <Helmet>
-      <title>EarOnDemand Blog | Get your stories and posts on tech solutions here</title>
+      <title>EarOnDemand | Get your stories and posts on tech solutions here</title>
      <meta name='keywords' content='earondemand earondemand blog earondemand-blog' />
-      <meta name='description' content='Here we give posts about everything in tech and business'></meta>
+      <meta name='description' content='Here we give posts about everything in tech and business'/>
       </Helmet>
     <div className='overflow-x-hidden w-full'>
         <Navbar/> 
